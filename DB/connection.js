4754944d59ce91a68b08b1754b40427890bc6d5d
Yara-Afaneh <yara.afaneh@gmail.com>
2024-02-24 20:38:47 +0200

    data base

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,7 @@
 import {Sequelize} from 'sequelize';
-export const sequelize = new Sequelize('blog-project', 'root', '', {
-    host: 'localhost',
+export const sequelize = new Sequelize('freedb_Blog-project', 'freedb_YaraAfaneh', '@gH7f23$Gcqz$G6', {
+    host: 'sql.freedb.tech',
+    port:3306,
     dialect:'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
   });
 
